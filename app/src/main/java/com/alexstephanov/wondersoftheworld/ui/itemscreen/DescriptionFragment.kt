0fb4ccd3b3624354b3edce61ec328ee79b5ee4a0
AlexStephanov <48553755+AlexStephanov@users.noreply.github.com>
2020-05-07 16:25:34 +0300

    Added text-to-speech.

--- app/src/main/java/com/alexstephanov/wondersoftheworld/ui/itemscreen/DescriptionFragment.kt
+++ app/src/main/java/com/alexstephanov/wondersoftheworld/ui/itemscreen/DescriptionFragment.kt
@@ -2,16 +2,22 @@ package com.alexstephanov.wondersoftheworld.ui.itemscreen
 
 import android.content.Context
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.alexstephanov.wondersoftheworld.R
 import com.alexstephanov.wondersoftheworld.listeners.OnFragmentEventListener
 import kotlinx.android.synthetic.main.fragment_description.*
 import kotlinx.android.synthetic.main.fragment_preview_1.*
+import java.util.*
 
-class DescriptionFragment(var listener: OnFragmentEventListener? = null, val description: String): Fragment() {
+class DescriptionFragment(private var listener: OnFragmentEventListener? = null, val description: String): Fragment(), TextToSpeech.OnInitListener {
+
+    private var textToSpeech: TextToSpeech? = null
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -32,9 +38,53 @@ class DescriptionFragment(var listener: OnFragmentEventListener? = null, val des
 
         description_detailed.text = description
 
+        textToSpeech = TextToSpeech(context, this)
+        button_start_text_to_speech.setOnClickListener {
+            speakOut(description)
+            button_stop_text_to_speech.visibility = View.VISIBLE
+            button_stop_text_to_speech.isClickable = true
+            button_start_text_to_speech.isClickable = false
+        }
+        button_stop_text_to_speech.setOnClickListener {
+            if(textToSpeech!!.isSpeaking) {
+                textToSpeech?.stop()
+                button_stop_text_to_speech.visibility = View.INVISIBLE
+                button_stop_text_to_speech.isClickable = false
+                button_start_text_to_speech.isClickable = true
+            }
+        }
+
         frame_layout_description.setOnClickListener{
             listener?.onBackgroundClickEvent()
         }
+    }
+
+    override fun onInit(status: Int) {
+        if(status == TextToSpeech.SUCCESS) {
+            if (textToSpeech?.isLanguageAvailable(Locale(Locale.getDefault().language))
+                == TextToSpeech.LANG_AVAILABLE
+            ) {
+                textToSpeech?.language = Locale(Locale.getDefault().language)
+            } else {
+                textToSpeech?.language = Locale.US
+            }
+            textToSpeech?.setPitch(1f)
+            textToSpeech?.setSpeechRate(1.1f)
+        } else if(status == TextToSpeech.ERROR) {
+            Toast.makeText(context, "Произошла ошибка", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun speakOut(text: String) {
+        textToSpeech?.speak(text, TextToSpeech.QUEUE_FLUSH, null)
+    }
+
+    override fun onDestroy() {
+        if(textToSpeech != null) {
+            textToSpeech!!.stop()
+            textToSpeech!!.shutdown()
+        }
+        super.onDestroy()
     }
 
 }
